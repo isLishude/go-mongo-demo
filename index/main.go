539012e16569eff503e71c2488b766b9b931b005
islishude <lishude@aliyun.com>
2018-10-16 15:42:22 +0800

    feat: update index

--- index/main.go
+++ index/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 
 	"github.com/islishude/demo/mongo/helper"
+	"github.com/islishude/demo/mongo/instance"
 	"github.com/mongodb/mongo-go-driver/bson"
 	"github.com/mongodb/mongo-go-driver/mongo"
 )
@@ -19,31 +20,38 @@ var dbName = "test"
 var url = "mongodb://127.0.0.1:27017"
 
 // IndexBuildDemo is
-// TODO: wait for driver fix IndexView export bug
 func IndexBuildDemo() {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	client, err := mongo.NewClient(url)
-	demoHelper.CheckError(err)
-
-	err = client.Connect(ctx)
-	demoHelper.CheckError(err)
-
-	// collection := client.Database(dbName).Collection(tableName)
+	defer demoMongo.MongoCancel()
 
 	index := mongo.IndexModel{
 		Keys:    bson.NewDocument(bson.EC.Int32("txid", int32(1))),
 		Options: mongo.NewIndexOptionsBuilder().Background(true).Build(),
 	}
 
-	// @TODO it's driver bug
-	// mongo.IndexView{Coll: collection}
-	res, err := new(mongo.IndexView).CreateOne(ctx, index)
+	res, err := demoMongo.MongoTrxCollection.Indexes().CreateOne(demoMongo.MongoCtx, index)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Println(res)
+}
+
+// GetIndexes is
+func GetIndexes() {
+	defer demoMongo.MongoCancel()
+
+	cur, err := demoMongo.MongoTrxCollection.Indexes().List(demoMongo.MongoCtx)
+	demoHelper.CheckError(err)
+
+	defer cur.Close(demoMongo.MongoCtx)
+
+	res := make(map[string]interface{})
+	for cur.Next(context.TODO()) {
+		cur.Decode(&res)
+	}
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%+v", res)
 }
